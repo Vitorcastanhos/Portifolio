da09cc9848e119b980ed9a3e01e6dd753b8fac3c
vitor <vitorcastanhos@gmail.com>
2023-08-20 20:37:56 -0300

    testing

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggle() {
+	const html = document.documentElement;
+
+	html.classList.toggle("light");
+
+	const img = document.querySelector(".profile img");
+
+	if (html.classList.contains("light")) {
+		img.setAttribute("src", "./assets/avatar-light.png");
+	} else {
+		img.setAttribute("src", "./assets/avatar.png");
+	}
+	// if(html.classList.contains('light')) {
+	//   html.classList.remove('light')
+	// } else {
+	//   html.classList.add('light')
+	// }
+}
