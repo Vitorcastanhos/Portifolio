436b5dce23fcc6c678591869be3d693a101f34c3
vitor <vitorcastanhos@gmail.com>
2023-12-23 15:55:47 -0300

    few more img changes

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function toggle() {
   const img = document.querySelector(".profile img");
 
   if (html.classList.contains("dark")) {
-    img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute("src", "./img/avatar.png");
   } else {
-    img.setAttribute("src", "./assets/");
+    img.setAttribute("src", "./img/avatar-light.png");
   }
 }
