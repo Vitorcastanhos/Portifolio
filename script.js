573d91f9062602e02a80acb1b72865ba67f63be7
vitor <vitorcastanhos@gmail.com>
2023-12-23 12:09:26 -0300

    teste

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ function toggle() {
 	if (html.classList.contains("light")) {
 		img.setAttribute("src", "./assets/avatar-light.png");
 	} else {
-		img.setAttribute("src", "./assets/avatar.png");
+		img.setAttribute("src", "./assets/");
 	}
 }
